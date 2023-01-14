fb12bcf4b5d5e164165c9ae5c18dd95a1d9a85b7
Roo Wiki <est.andres.pineros@unimilitar.edu.co>
2023-01-13 22:46:20 -0500

    Create FigurasBasicas.py
    Generar figuras basicas

---
+++ FigurasBasicas.py
@@ -0,0 +1,9 @@
+# introduccion 
+"""Generar figuras basicas"""
+cmds.polySphere()# Crear un esfera 
+cmds.polyCube()# Crear un cubo 
+cmds.polyCylinder()# Crear un cilindo 
+cmds.polyCone()# Crear un cono
+cmds.polyTorus()# Crear un toroide
+cmds.polyPlane()# Crear un Plano
+cmds.polyDisc()# Crear un disco
