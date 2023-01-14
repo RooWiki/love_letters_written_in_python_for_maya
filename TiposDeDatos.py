5749c89c38619a2bb8045ca7aef0c37d99999ce3
Roo Wiki <est.andres.pineros@unimilitar.edu.co>
2023-01-14 00:41:29 -0500

    Create TiposDeDatos.py

---
+++ TiposDeDatos.py
@@ -0,0 +1,36 @@
+#TIPOS DE DATOS
+
+#STRING. Una cadean de texto
+"Esto es un string"
+type("este es un strin")
+
+#INT. un numero ENTERO 
+1
+10
+478
+45
+type (10)
+
+#FLOAT. un numero con decimales
+10.5
+4.98
+5.098
+type (10.5)
+
+#BOOL. un tipo de dato binareos  
+True 
+False
+0
+1
+type(True)
+
+#Estructura de datos
+#TUPLA. un conjunto de datos INMUTABLE
+("uno","dos","tres")
+("texto", 10, True, 10.5)
+type (("Texto", 2, True))
+
+#LISTA. un conjutno de datos MUTABLE
+["uno","dos","tres"]
+["texto", 10, True, 10.5]
+type (["uno","dos","tres"])
