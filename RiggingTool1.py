1c00a52de834c34f7a20417ddca1f023203157e8
Roo Wiki <est.andres.pineros@unimilitar.edu.co>
2023-02-08 11:14:50 -0500

    Update RiggingTool1.py

--- RiggingTool1.py
+++ RiggingTool1.py
@@ -16,33 +16,33 @@ window_title = "Simple Rigg by RooWiki"
 window_w = 200
 window_h = 168
 
-def create_window(): 
+def create_window():
     if cmds.window(window_name, query=True, exists=True):
         cmds.deleteUI(window_name)
     cmds.window(window_name)
     cmds.window(window_name, edit=True, w=window_w+2, h=window_h, title=window_title, mnb=False, mxb=False,  s=False)
-    
+   
     cmds.columnLayout("main_colum", w=window_w, h=window_h)
     create_customUI()
-    
+   
     cmds.showWindow(window_name)
-    
+   
 def create_customUI():
     cmds.button("rigg_button", label="Rigg", w = window_w, h=40, command=rigg)
-    
+   
     cmds.frameLayout("cuntrol_scale_frame", label="Control Scale", w=window_w, parent="main_colum", cll=True, cl=True, bgc=(.250,.160,.255))
     cmds.rowLayout("control_scale_row", nc=3, w=window_w)
-    
+   
     cmds.floatField(precision=2, w=window_w/3)
     cmds.button("samll_burron", label="Samll", w=window_w/3)
     cmds.button("bugg_button", label="Big", w=window_w/3)
-    
+   
     cmds.frameLayout("control_color_frame", label="Control Color", w=window_w, parent="main_colum", cll=True, cl=True, bgc=(0.5,0.5,0.0))
     cmds.colorIndexSliderGrp(max=60, v=50)
     cmds.button("set_color", label="Set Color", h=30)
-    
-    
-    
+   
+   
+   
 # FUNCTIONS
 def rigg(*args):
 
@@ -56,7 +56,7 @@ def rigg(*args):
             if cmds.objectType(selection[0])!="joint":
                 cmds.warning("Por favor selecione un joint")
                 return
-                
+               
         if len(selection) == 2:
             shape = cmds.listRelatives(selection[1],children=True)[0]
             if cmds.objectType(selection[0])=="joint" and cmds.objectType(shape)=="nurbsCurve":
@@ -64,43 +64,43 @@ def rigg(*args):
             else:
                 cmds.warning("Por favor selecione un joint y luego una curva")
                 return
-                
+               
         selected_joint = selection[0]
         cmds.select(selected_joint,hi=True)
         all_joint = cmds.ls(selection=True)
-        
+       
         control_list = []
         grp_list = []
         control_scale = 1
-            
+           
         for n in range(len(all_joint)-1):
-            
+           
             if len(selection) == 1:
-                newcontrol = cmds.circle(name=all_joints[n]+"_ctrl",radius = control_scale)[0] 
+                newcontrol = cmds.circle(name=all_joint[n]+"_ctrl",radius = control_scale)[0]
                 control_list.append(newcontrol)
                 cmds.rotate(0,90,0, newcontrol)
                 cmds.scale(3,3,3, newcontrol)
             if len(selection) == 2:
                 newcontrol = cmds.duplicate(selection[1])[0]
                 control_list.append(newcontrol)
-                
+               
             cmds.makeIdentity(apply=True)
             cmds.DeleteHistory()
-            
+           
             grp = cmds.group(name = newcontrol + "_grp", empty=True)
             grp_list.append(grp)
             cmds.parent(newcontrol, grp)
-            
-            joint_position = cmds.xform(all_joints[n], query=True, translation=True, worldSpace=True)
+           
+            joint_position = cmds.xform(all_joint[n], query=True, translation=True, worldSpace=True)
             cmds.xform(grp, translation=joint_position)
-            orient_cons = cmds.orientConstraint(all_joints[n], grp)
+            orient_cons = cmds.orientConstraint(all_joint[n], grp)
             cmds.delete(orient_cons)
-            
-            cmds.parentConstraint(newcontrol,all_joints[n])
-            
+           
+            cmds.parentConstraint(newcontrol,all_joint[n])
+           
         for j in range(len(all_joint)-2):
             cmds.parent(grp_list[j+1], control_list[j])
-    
-    
-    
+   
+   
+   
 create_window()
